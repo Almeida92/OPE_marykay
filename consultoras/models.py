c7698786c98e230f5c8d4e7fb522212aebcbf57e
Felipe almeida <felipepereira937@gmail.com>
2018-08-16 19:41:09 -0300

    Criando Model consultora

--- consultoras/models.py
+++ consultoras/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Consultora(models.Model):
+  usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+  nivel = models.CharField(max_length=200)
+  sexo = models.CharField(max_length=1)
+  status = models.CharField(max_length=200)
+  cpf = models.CharField(max_length=200)
+  data_nascimento = models.DateTimeField('Data de nascimento')
+
