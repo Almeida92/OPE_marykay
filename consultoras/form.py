df74b959a39fe7ea5bec8c556d4f9c993c374f8f
Felipe almeida <felipepereira937@gmail.com>
2018-08-29 14:30:23 -0300

    formulario de consultora

--- consultoras/form.py
+++ consultoras/form.py
@@ -11,3 +11,38 @@ class ConsultoraForm(forms.Form):
         max_length=100,
         widget=forms.TextInput(attrs={'class' : 'form-control'}))
     sexo = forms.ChoiceField(choices=[('f','(F)'), ('m','(M)')], widget=forms.RadioSelect())
+    endereco = forms.CharField(
+        label='Endereço',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    bairro = forms.CharField(
+        label='Bairro',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    cidade = forms.CharField(
+        label='Cidade',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    dt_nascimento = forms.DateField(label= 'Data de Nascimento', widget=forms.TextInput(
+        attrs={'class' : 'form-control', 'type': 'date'}))
+    telefone = forms.CharField(
+        label='Telefone',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    celular = forms.CharField(
+        label='Celular',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    status = forms.CharField(
+        label='status',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    nivel = forms.CharField(
+        label='nivel',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    desconto = forms.CharField(
+        label='Desconto',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    
