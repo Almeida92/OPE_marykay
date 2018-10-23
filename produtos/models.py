2151b2a65277f8454d67370e8f7df666bcfa07c7
Felipe almeida <felipepereira937@gmail.com>
2018-10-23 20:22:23 -0300

    Criando model de produtos

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+  codigo = models.IntegerField(default= 0 )
+  descricao = models.CharField(max_length=100, default= '')  
+
+class Produto(models.Model):
+  codigo = models.CharField(max_length=100, default= '')
+  nome = models.CharField(max_length=100, default= '')
+  preco = models.DecimalField(max_digits=6, decimal_places=2)
+  categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="usuario")
+  
