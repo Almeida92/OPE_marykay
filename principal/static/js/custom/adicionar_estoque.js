9d341197c1d358ebdb03a751c3d29044276640ae
Felipe almeida <felipepereira937@gmail.com>
2018-10-26 15:50:05 -0300

    Adicionar estoque JS

---
+++ principal/static/js/custom/adicionar_estoque.js
@@ -0,0 +1,16 @@
+function adicionar_estoque(produto, usuario) {
+  quantidade = $(`#quantidade-produto-${produto}`).val();
+  swal({
+      title: "Deseja realmente sair do sistema ?",
+      text: "",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonClass: 'btn-danger',
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'não',
+      closeOnConfirm: false,
+    },
+    function () {
+      window.location.replace(`/estoque/inserir/${produto}/${usuario}/${quantidade}`);
+    });
+}
