155c22ec6180981418525364eb2c24753a96a085
Felipe almeida <felipepereira937@gmail.com>
2018-11-20 16:24:21 -0200

    LENDO O ARQUIVO

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from openpyxl import load_workbook
+import os.path
 
 def importar_pedidos(request):
-    folder='tmp/'
     if request.method == 'POST':
         myfile = request.FILES['file']
-        fs = FileSystemStorage(location=folder) #defaults to   MEDIA_ROOT  
-        filename = fs.save(myfile.name, myfile)
-        file_url = fs.url(filename)
+        salvar_arquivo(myfile)
     else:
       pass
       
     return render(request, 'pedidos/importar.html')
+
+def salvar_arquivo(arquivo):
+  folder='tmp/'
+  fs = FileSystemStorage(location=folder)
+  filename = fs.save(arquivo.name, arquivo)
+  file_url = fs.url(filename)
+  ler_arquivo(file_url)
+
+def ler_arquivo(nome_arquivo):
+  # print(os.path.dirname())
+  wb = load_workbook(filename = 'tmp/' + nome_arquivo)
+  sheets = wb.sheetnames
+  print(sheets)
